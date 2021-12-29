9b415a3b2e856500ca162743f507a625e25602af
kwoksiuwang <576713880@qq.com>
2021-12-29 20:57:20 +0800

    修改注释

--- app/src/main/java/com/kwoksiuwang/designmode/statemode/ISceneState.kt
+++ app/src/main/java/com/kwoksiuwang/designmode/statemode/ISceneState.kt
@@ -12,7 +12,7 @@ public abstract class ISceneState(val stateMachine: StateMachine) {
     //状态被终止时被调用
     public abstract fun stateEnd()
 
-    //状态的通用行为
+    //状态的通用行为。每n秒被调用一次，模拟垂直同步或者unity中的onUpdate
     public abstract fun stateUpdate()
 
     //状态的通用行为
